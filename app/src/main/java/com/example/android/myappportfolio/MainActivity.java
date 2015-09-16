60392bf6e88aeda576be1ec8abb41420e634c58b
Ali Thabet <alitabet@gmail.com>
2015-09-16 16:08:47 +0300

    Finish My App Portfolio main page

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyAppPortfolioActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +41,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Called when the user touches the button
+     * Called when the user touches a button
      */
     public void sendMessage(View view) {
-        String forecast = "This button will launch an app";
+        // First we fidn the button clicked
+        Button clickedButton = (Button) findViewById(view.getId());
+        // We extract the button text
+        String buttonText = clickedButton.getText().toString();
+        // Extra: To enhaced readability, we add the word app
+        // at the end of the button text
+        String[] temp = buttonText.split(" ");
+        if (!temp[temp.length - 1].equals("app")) {
+            buttonText += " app";
+        }
+        String forecast = getString(R.string.toast_launch_app)
+                        + " "
+                        + buttonText.toUpperCase();
         Toast.makeText(getApplicationContext(), forecast, Toast.LENGTH_SHORT).show();
     }
 }
